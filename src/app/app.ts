a97aad1a8abc8eb13b01d83c220bbc81aacff92c
vinivizzapc <vini_vizza@hotmail.com>
2026-02-10 19:55:32 -0300

    Inicializando o projeto web-commerce

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AuthPage } from './auth-page/auth-page';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [AuthPage],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('projeto-webcommerce');
---
+++ src/app/auth-page/auth-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { LoginForm } from './login/login-form';
+import { RegisterForm } from './register-form/register-form';
+
+@Component({
+  selector: 'app-auth-page',
+  imports: [LoginForm, RegisterForm],
+  templateUrl: './auth-page.html',
+  styleUrl: './auth-page.css',
+})
+export class AuthPage {}
