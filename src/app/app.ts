4989e18cc035f262477bc5715bcef06f8b50055f
Beatriz Silva Dev <biasilvadev1109@gmail.com>
2026-02-10 21:58:34 -0300

    Home page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { AuthPage } from './auth-page/auth-page';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [AuthPage],
+  imports: [AuthPage, HomePageComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
