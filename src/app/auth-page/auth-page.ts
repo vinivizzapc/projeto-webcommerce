2d76e8edf6bbd1453d351697c2a88ca54cbcaaf4
vinivizzapc <vini_vizza@hotmail.com>
2026-02-10 21:23:29 -0300

    Subindo dependencia nova de icons

--- src/app/auth-page/auth-page.ts
+++ src/app/auth-page/auth-page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { LoginForm } from './login/login-form';
+import { LoginForm } from './login-form/login-form';
 import { RegisterForm } from './register-form/register-form';
 
 @Component({
