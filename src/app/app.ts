bb27778fc248172fb488a1623e435d4ee0f30e34
Beatriz Silva Dev <biasilvadev1109@gmail.com>
2026-02-11 23:15:57 -0300

    Alteração home page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { AuthPage } from './auth-page/auth-page';
 import { HomePageComponent } from './home-page/home-page.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule} from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [AuthPage, HomePageComponent],
+  imports: [AuthPage, HomePageComponent, MatToolbarModule, MatIconModule, MatFormFieldModule, MatInputModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatInputModule} from '@angular/material/input';
+
+
 
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [MatToolbar, MatIconModule, MatFormFieldModule, MatInputModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
